a75fa14d9e01ad4f262d0a79b97080e6d078f747
dbeeke <daniel@studiofonkel.nl>
2015-03-09 20:18:47 +0100

    mm

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,38 @@
 $(function() {
 
-  new Dragdealer('slides', {
+  var slider = new Dragdealer('slides', {
     steps: 2,
-    speed: 0.3,
+    speed: 0.5,
     loose: true,
     handleClass: 'slides-handle',
-    requestAnimationFrame: true
+    requestAnimationFrame: true,
   })
 
-  $('.mainmenu-link').on('click', function () {
-    $('body').doTransition('mainmenu-link', '.slides')
-
+  $('.mainmenu-link').on('click', function (e) {
+    $(this).addClass('mainmenu-link--is-active')
+    $(this).doTransition('mainmenu-link', function () {
+      console.log($(this).attr('href'))
+    })
 
     return false
   })
 
-  jQuery.fn.doTransition = function($className, $object) {
-    $(this).addClass('has--' + $className + '--transition')
+
+  $('.mainmenu-link').on('mousedown', function (e) {
+    console.log('test')
+  })
+
+  jQuery.fn.doTransition = function($className, $callback) {
+    $('body').addClass('has--' + $className + '--transition')
 
     return this.hide(0, function() {
       $(this).show()
-      $(this).addClass('is--' + $className + '--transition')
+      $('body').addClass('is--' + $className + '--transition')
 
-      $($object).one('transitionend', function () {
-        alert('test')
+      $(this).one('transitionend', function () {
+        if (typeof $callback == 'function') {
+          $callback()
+        }
       })
     })
   }
