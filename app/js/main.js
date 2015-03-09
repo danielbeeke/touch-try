8a88b36f15ea69df47cee969049f78e733c75248
dbeeke <daniel@studiofonkel.nl>
2015-03-09 01:20:39 +0100

    going to bed

--- app/js/main.js
+++ app/js/main.js
@@ -8,4 +8,24 @@ $(function() {
     requestAnimationFrame: true
   })
 
+  $('.mainmenu-link').on('click', function () {
+    $('body').doTransition('mainmenu-link', '.slides')
+
+
+    return false
+  })
+
+  jQuery.fn.doTransition = function($className, $object) {
+    $(this).addClass('has--' + $className + '--transition')
+
+    return this.hide(0, function() {
+      $(this).show()
+      $(this).addClass('is--' + $className + '--transition')
+
+      $($object).one('transitionend', function () {
+        alert('test')
+      })
+    })
+  }
+
 });
