33b2766420d57bc7482dd90521e60d389ca6ee1e
dbeeke <daniel@studiofonkel.nl>
2015-03-21 16:19:58 +0100

    slider is smoother

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,6 @@ $(function() {
   .on('mousedown', startDrag)
   .on('mouseup', endDrag)
   .on('mousemove', doDrag)
-  .on('transitionend', removeTransition)
 
   var dragging = false
   var startX = 0
@@ -21,22 +20,21 @@ $(function() {
   var resistancePoint = 800
   var resistanceSpeed = .3
   var menuItemCount = $('.mainmenu-link').length
-
+  var elementStartedWith
 
   function startDrag(e) {
     removeTransition()
     dragging = true
     $('body').addClass('is--dragging')
-    startX = e.clientX
-    // $('.mainmenu-link').removeAttr('style')
-    // $('body').removeClass('has--active-mainmenu-link')
-    // $('.mainmenu-link--is-dragged').removeClass('mainmenu-link--is-dragged')
+    startX = e.screenX
+    elementStartedWith = e.target
   }
 
   function endDrag(e) {
+    element.css('transition', 'all .5s ease')
     dragging = false
     $('body').removeClass('is--dragging')
-    endX = e.clientX
+    endX = e.screenX
 
     var newPoint = 0
     var diff = startX - endX
@@ -48,6 +46,8 @@ $(function() {
       slideIndex = slideIndex - 1
     }
 
+    element.css('transform', 'translate3d(' + (((slideIndex * -1) * windowWidth) + newPoint) + 'px, 0, 0)')
+
     if ($('.mainmenu-link--is-dragged').length && diff > flipPoint) {
       $('.mainmenu-link--is-dragged').addClass('mainmenu-link--is-active')
       $('body').addClass('has--active-mainmenu-link')
@@ -58,9 +58,6 @@ $(function() {
       $('.mainmenu-link--is-dragged').removeClass('mainmenu-link--is-dragged')
     }
 
-    element.css('transition', 'all .5s ease')
-    element.css('transform', 'translate3d(' + (((slideIndex * -1) * windowWidth) + newPoint) + 'px, 0, 0)')
-
   }
 
   function removeTransition() {
@@ -70,24 +67,23 @@ $(function() {
   function doDrag(e) {
     if (dragging) {
 
-      currentX = e.clientX
+      currentX = e.screenX
 
       var newPoint = 0
       var diff = startX - currentX
 
       if (diff > 0 && slideIndex < (slideCount - 1) || diff < 0  && slideIndex > 0) {
-        element.css('transform', 'translate3d(' + (((slideIndex * -1) * windowWidth) + e.clientX - startX) + 'px, 0, 0)')
+        element.css('transform', 'translate3d(' + (((slideIndex * -1) * windowWidth) + e.screenX - startX) + 'px, 0, 0)')
       }
       else if (resistancePoint > Math.abs(diff)) {
-        element.css('transform', 'translate3d(' + (((slideIndex * -1) * windowWidth) + (e.clientX - startX) * resistanceSpeed) + 'px, 0, 0)')
+        element.css('transform', 'translate3d(' + (((slideIndex * -1) * windowWidth) + (e.screenX - startX) * resistanceSpeed) + 'px, 0, 0)')
       }
 
-
-      if ($(e.target).hasClass('mainmenu-link')) {
-        $('.mainmenu-link').css('transition', 'opacity .4s ease-in-out')
+      if ($(elementStartedWith).hasClass('mainmenu-link')) {
+        $('.mainmenu-link').css('transition', 'opacity .5s ease-in-out')
 
         $('.mainmenu-link--is-dragged').removeClass('mainmenu-link--is-dragged')
-        $(e.target).addClass('mainmenu-link--is-dragged')
+        $(elementStartedWith).addClass('mainmenu-link--is-dragged')
 
         var maxPercentageToGive = 1 - (1 / menuItemCount)
         var dragPercentage = diff / (resistancePoint / 100)
